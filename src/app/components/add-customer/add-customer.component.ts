744ad4e35017133de2e1326c176531781de90c6d
Balaji <balaji.vishnubhotla@scadea.com>
2024-01-08 10:09:03 -0600

    Add New Customer Implemented

--- src/app/components/add-customer/add-customer.component.ts
+++ src/app/components/add-customer/add-customer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ICustomer, getDefaultCustomer } from 'src/app/models/customer.model';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-add-customer',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-customer.component.scss']
 })
 export class AddCustomerComponent {
+  customer: ICustomer = getDefaultCustomer();
+  submitted = false;
 
+  constructor(private customerService: CustomerService) { }
+
+  saveCustomer(): void {
+    const newCustomer: ICustomer = {
+      ...getDefaultCustomer(),
+      firstName: this.customer.firstName,
+      lastName: this.customer.lastName,
+      email: this.customer.email
+    };
+
+    this.customerService.create(newCustomer)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newCustomer(): void {
+    this.submitted = false;
+    this.customer = getDefaultCustomer();
+  }
 }
